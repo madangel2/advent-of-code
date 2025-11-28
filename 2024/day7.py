db33754ff794d6a69da7ef0c5c4dbbb0d9f743f2
Maxime Paquette <mxp1001@gmail.com>
2025-11-28 13:28:44 -0500

    day7 optimization

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,29 +1,48 @@
 from utils import get_data
 from operator import add, mul
 
-def concat(x,y):
-    return int(f"{x}{y}")
+def concat(x, y):
+    if y < 10:
+        return x * 10 + y
+    elif y < 100:
+        return x * 100 + y
+    elif y < 1000:
+        return x * 1000 + y
+    else:
+        digits = len(str(y))
+        return x * (10 ** digits) + y
 
 def check_equation(nums, ops):
     if len(nums) == 2:
         return nums[0] == nums[1]
 
     total, a, b, *rest = nums
-
+    
     for op in ops:
-        if check_equation([total, op(a, b)] + rest, ops):
-            return total
+        result = op(a, b)
+        if result <= total or op == concat:
+            if check_equation([total, result] + rest, ops):
+                return total
     return 0
 
-    
-    
 def solve():
     data = get_data(7)
     problems = [list(map(int, line.replace(':','').split())) for line in data.splitlines()]
 
-    part1 = sum(check_equation(problem, ops=[add,mul]) for problem in problems)
-    part2 = sum(check_equation(problem, ops=[add,mul,concat]) for problem in problems)
+    part1_ops = [add, mul]
+    part1_result = 0
+    unsolved = []
     
-    return part1, part2
+    for problem in problems:
+        result = check_equation(problem, ops=part1_ops)
+        if result:
+            part1_result += result
+        else:
+            unsolved.append(problem)
+    
+    part2_ops = [add, mul, concat]
+    part2_additional = sum(check_equation(problem, ops=part2_ops) for problem in unsolved)
+    
+    return part1_result, part1_result + part2_additional
 
 
