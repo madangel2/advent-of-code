629c7717658e83b4dd9f81947bbf5ad86e802458
Maxime Paquette <mxp1001@gmail.com>
2025-11-28 16:12:54 -0500

    day 22 optimization

--- 2024/day22.py
+++ 2024/day22.py
@@ -11,27 +11,32 @@ def solve():
     data = get_data(22)
     buyerSecrets = [int(line) for line in data.splitlines()]
     total = 0
-    occurences = defaultdict(dict)
-
+    
+    # Track total bananas for each sequence across all buyers
+    sequence_totals = defaultdict(int)
+    
     for secret in buyerSecrets:
         lastVariations = deque(maxlen=4)
-        originalSecret = secret
-        lastSecret = secret
+        seen_sequences = set()  # Track sequences seen for this buyer
+        lastPrice = secret % 10
+        
         for i in range(2000):
             secret = calcNextSecret(secret)
-            lastPrice = lastSecret % 10
             currentPrice = secret % 10
             priceDiff = currentPrice - lastPrice
             lastVariations.append(priceDiff)
-            if len(lastVariations) == 4:
+            
+            # Only process when we have a full sequence of 4
+            if i >= 3:  # After 4 iterations (0,1,2,3), we have 4 diffs
                 key = tuple(lastVariations)
-                if originalSecret not in occurences[key]:
-                    occurences[key][originalSecret] = currentPrice          
-            lastSecret = secret
+                # Only count first occurrence of this sequence for this buyer
+                if key not in seen_sequences:
+                    seen_sequences.add(key)
+                    sequence_totals[key] += currentPrice
+            
+            lastPrice = currentPrice
         total += secret
-
-    sequence = max(occurences, key=lambda k: sum(occurences[k].values()))
     
     part1 = total
-    part2 = sum(occurences[sequence].values())
+    part2 = max(sequence_totals.values())
     return part1, part2
