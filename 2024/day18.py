b093f080c49badd39b9b09cc1c9bd90d5d294bb3
Maxime Paquette <mxp1001@gmail.com>
2025-11-28 13:57:05 -0500

    day 18 optimization

--- 2024/day18.py
+++ 2024/day18.py
@@ -14,32 +14,43 @@ def initGraph(map):
                         
     return mapGraph
 
+def buildCorruptedMap(corruptedMemory, nbCorruptedMemory, mapWidth, mapHeight):
+    map = initMap(mapWidth, mapHeight, '.')
+    for i in range(nbCorruptedMemory):
+        map[corruptedMemory[i]] = '#'
+    return map
 
-# Init challenge
+def getLowestPathToEnd(map, startPos, endPos):
+    g = initGraph(map)
+    lowestScore, path = g.getLowestScores(startPos)
+    return lowestScore[endPos]
+
 def solve():
     data = get_data(18)
     corruptedMemory = [complex(int(line.split(",")[0]), int(line.split(",")[1]))for line in data.splitlines()]
     mapWidth = 70
-    mapHeight = mapWidth
-    map = initMap(mapWidth, mapHeight, '.')
+    mapHeight = 70
     startPos = complex(0, 0)
     endPos = complex(mapWidth, mapHeight)
+    
+    # Part 1
+    map = buildCorruptedMap(corruptedMemory, 1024, mapWidth, mapHeight)
+    part1 = getLowestPathToEnd(map, startPos, endPos)
 
-    part1 = None
+    # Part 2
+    left, right = 1024, len(corruptedMemory) - 1
     part2 = None
 
-    for i in range(len(corruptedMemory)):
-        map[corruptedMemory[i]] = '#'
+    while left <= right:
+        mid = (left + right) // 2
         
-        if(i >= 1023):
-            g = initGraph(map)
-            lowestScore, path = g.getLowestScores(startPos)
-
-            #TODO cleanup and speedup
-            if(i == 1023):
-                part1 = lowestScore[endPos]
-            elif(lowestScore[endPos] == float('inf')):
-                part2 = corruptedMemory[i]
-                break
+        map = buildCorruptedMap(corruptedMemory, mid + 1, mapWidth, mapHeight)
+        lowestPathToEnd = getLowestPathToEnd(map, startPos, endPos)
+        
+        if lowestPathToEnd == float('inf'):
+            part2 = corruptedMemory[mid]
+            right = mid - 1
+        else:
+            left = mid + 1
     
     return part1, part2
